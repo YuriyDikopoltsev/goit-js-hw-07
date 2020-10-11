8b20477fa655efc8080c83b3a220b30e356b352d
Yuriy Dikopoltsev <YuriyDikopoltsev@gmail.com>
2020-10-12 00:56:25 +0300

    2 tsk solved

--- js/task-2.js
+++ js/task-2.js
@@ -1,25 +1,18 @@
-const calculateEngravingPrice = (message = '', pricePerWord = 0) =>
-  message.split(' ').length * pricePerWord; // Write code in this line
-console.log(
-  calculateEngravingPrice(
-    'Proin sociis natoque et magnis parturient montes mus',
-    10,
-  ),
-); // 80
-
-console.log(
-  calculateEngravingPrice(
-    'Proin sociis natoque et magnis parturient montes mus',
-    20,
-  ),
-); // 160
-
-console.log(
-  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-); // 240
-
-console.log(
-  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-); // 120
-
-console.log(calculateEngravingPrice('Uno', 100)); // 100
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+const ingredientsContainerRef = document.querySelector('#ingredients');
+const makeIngredientList = ingredients => {
+  return ingredients.map(ingredient => {
+    const ingredientRef = document.createElement('li');
+    ingredientRef.textContent = ingredient;
+    return ingredientRef;
+  });
+};
+const references = makeIngredientList(ingredients);
+ingredientsContainerRef.append(...references);
