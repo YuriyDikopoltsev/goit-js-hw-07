386d55fcb2a21c0c9e1b0d6398f16ac2e1049775
Yuriy Dikopoltsev <YuriyDikopoltsev@gmail.com>
2020-10-17 20:15:57 +0300

    6-th task done

--- js/task-6.js
+++ js/task-6.js
@@ -1,16 +1,30 @@
-function mapArray(array) {
-  'use strict';
-  const numbers = new Array(array.length);
-  for (let i = 0; i < array.length; i += 1) {
-    // console.log(array[i]);
-    array.splice(i, 1, array[i] * 10);
+// Напиши скрипт, который бы при потере фокуса на инпуте, проверял его содержимое на правильное количество символов.
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Введи 6 символов"
+// />;
+
+// #validation-input {
+//   border: 3px solid #bdbdbd;
+// }
+
+// #validation-input.valid {
+//   border-color: #4caf50;
+// }
+
+// #validation-input.invalid {
+//   border-color: #f44336;
+// }
+
+const inputRef = document.querySelector('#validation-input');
+// console.log(inputRef.dataset.length);
+inputRef.addEventListener('blur', onInputBlur);
+function onInputBlur(event) {
+  if (event.currentTarget.value.length < inputRef.dataset.length) {
+    inputRef.style.borderColor = '#f44336';
+  } else {
+    inputRef.style.borderColor = '#4caf50';
   }
-  console.table(array);
-  return array;
 }
-
-console.log(mapArray([-2, 0, 2]));
-// [-20, 0, 20]
-
-console.log(mapArray([-2.5, 0, 2.5]));
-// [-25, 0, 25]
